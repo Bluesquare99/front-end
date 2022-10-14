9d436bbaa597ac521e9b6cc86a216319b4a21de1
Max McCready <msmccready@gmail.com>
2022-10-13 19:53:23 -0500

    maybe this is different

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	ssr: {
-		noExternal: ['three', 'troika-three-text'],
-		external: ['three-csg-ts']
+		noExternal: ['three', 'troika-three-text']
 	}
 };
 
